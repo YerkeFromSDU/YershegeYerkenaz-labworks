5f8fc1b388187c6c958531c0617530722a66e4f5
YerkeFromSDU <190103406@stu.sdu.edu.kz>
2021-03-15 20:32:34 +0600

    blog/index is created

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 use App\Models\Post;
-
+use App\Http\Controllers\BlogController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -26,8 +26,10 @@ Route::get('post/create', function () {
     ]);
 });
 
-Route::get('post', function () {
-    $post = Post::find(1);
-    return $post;
-});
+// Route::get('post', function () {
+//     $post = Post::find(1);
+//     return $post;
+// });
+
+Route::get('post', [BlogController::class, 'index']);
 
